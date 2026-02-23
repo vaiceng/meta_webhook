73eed3204e347af09b9bb3f5bb100fb2fbf3b851
vicente guillen <vigaymer48@gmail.com>
2026-02-23 19:41:01 -0300

    Fix console log syntax in webhook handler

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-  console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log(\n\nWebhook received ${timestamp}\n);
   console.log(JSON.stringify(req.body, null, 2));
   res.status(200).end();
 });
